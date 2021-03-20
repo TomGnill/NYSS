d0de326863c46723efda8b5e1b159c78cac60845
TomGnill <s.drun@yandex.ru>
2021-03-20 11:43:49 +0300

    Добавьте файлы проекта.

---
+++ NYSS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NYSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
